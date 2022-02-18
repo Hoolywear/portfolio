a1a6dad29285fc5fe2d18374cd613a4b17e8043c
Hoolywear <iacopo@alessandroruzzier.it>
2022-02-18 18:14:25 +0100

    temporarily removed color randomness for main links, changed title font and position

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-var links = document.getElementsByClassName('main-link');
-
 var winWidth = window.innerWidth;
 var winHeight = window.innerHeight;
 
 var mainLinks = [
-  [document.getElementById('main-link-1'), 25, 35, 'red'],
-  [document.getElementById('main-link-2'), 25, 65, 'green'],
-  [document.getElementById('main-link-3'), 75, 35, 'blue'],
-  [document.getElementById('main-link-4'), 75, 65, 'yellow'],
+  [document.getElementById('main-link-1'), 25, 35],
+  [document.getElementById('main-link-2'), 25, 65],
+  [document.getElementById('main-link-3'), 75, 35],
+  [document.getElementById('main-link-4'), 75, 65],
 ];
 
-var colors = ['red','green','blue','yellow'];
+var colors = [['#E54B4B','white'],['#48483A','white'],['#247BA0','white'],['#FAC748','black']];
+var colors2 = [['#003b01','white'],['white','#003b01']];
 
 for ( var i=0; i < mainLinks.length; i++ ) {
 
@@ -21,8 +20,9 @@ for ( var i=0; i < mainLinks.length; i++ ) {
     randomLeft = getRandomNumber(-5, 5);
     console.log(randomTop, randomLeft);
 
-    randomColor = getRandomNumber(0,4);
-    thisDiv.style.background = colors[Math.floor(randomColor)];
+    randomColor = getRandomNumber(0,2);
+    //thisDiv.style.background = colors2[Math.floor(randomColor)][0];
+    //thisDiv.style.color = colors2[Math.floor(randomColor)][1];
 
     endTop = randomTop + mainLinks[i][1];
     endLeft = randomLeft + mainLinks[i][2];
