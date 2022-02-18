88cf7ccbc41fe4cbfd7d77e7c5d00dbd93a4be9c
Hoolywear <iacopo@alessandroruzzier.it>
2022-02-18 17:36:21 +0100

    fine-tuned random position functionality for main titles. added color randomness and on hover behaviour

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-var mainLinks = document.getElementsByClassName('main-link');
+var links = document.getElementsByClassName('main-link');
 
 var winWidth = window.innerWidth;
 var winHeight = window.innerHeight;
 
+var mainLinks = [
+  [document.getElementById('main-link-1'), 25, 35, 'red'],
+  [document.getElementById('main-link-2'), 25, 65, 'green'],
+  [document.getElementById('main-link-3'), 75, 35, 'blue'],
+  [document.getElementById('main-link-4'), 75, 65, 'yellow'],
+];
+
+var colors = ['red','green','blue','yellow'];
+
 for ( var i=0; i < mainLinks.length; i++ ) {
 
-    var thisDiv = mainLinks[i];
+    var thisDiv = mainLinks[i][0];
+    console.log(mainLinks[i][0]);
 
-    randomTop = getRandomNumber(0, winHeight);
-    randomLeft = getRandomNumber(0, winWidth);
+    randomTop = getRandomNumber(-5, 5);
+    randomLeft = getRandomNumber(-5, 5);
+    console.log(randomTop, randomLeft);
 
-    thisDiv.style.top = randomTop +"px";
-    thisDiv.style.left = randomLeft +"px";
+    randomColor = getRandomNumber(0,4);
+    thisDiv.style.background = colors[Math.floor(randomColor)];
 
+    endTop = randomTop + mainLinks[i][1];
+    endLeft = randomLeft + mainLinks[i][2];
+    console.log(endTop, endLeft);
+
+    thisDiv.style.top = endTop +"vh";
+    thisDiv.style.left = endLeft +"vw";
+    console.log(thisDiv.style.top, thisDiv.style.left);
 }
 
 function getRandomNumber(min, max) {
